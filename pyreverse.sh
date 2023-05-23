9dcb04865ddfe8cb5468cab81e06b70d8e4d28d8
Mark Dyer <heymarky@gmail.com>
2023-05-22 18:20:51 -0700

    Add more packages to class diagram generator

--- pyreverse.sh
+++ pyreverse.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-/Users/mark/Library/Python/3.8/bin/pyreverse -o pdf ./threads.py ./camera.py ./config.py ./motion.py
+/Users/mark/Library/Python/3.8/bin/pyreverse -o pdf ./threads.py ./camera.py ./config.py ./motion.py \
+  ./camera_producer.py ./scheduling.py
 open classes.pdf
