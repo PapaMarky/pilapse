f7125d976acffca54c105a95df356fcbedbef7de
Mark Dyer <heymarky@gmail.com>
2023-07-30 16:49:46 -0700

    Precompute the contours for the dark frame
    Faster than re-computing for each frame

--- pilapse/darkframe.py
+++ pilapse/darkframe.py
@@ -5,17 +5,21 @@ import logging
 
 from pilapse import colors
 
-def apply_darkframe(image, darkimage, threshold=6, highlight=False):
-    outimage = image.copy()
-    # find the 'average' color of the input image
-    avg_color_per_row = numpy.average(image, axis=0)
-    avg_color = numpy.average(avg_color_per_row, axis=0)
-
+def get_contours(darkimage, threshold=6):
     # get locations of bad pixels
     darkgray = cv2.cvtColor(darkimage, cv2.COLOR_BGR2GRAY)
     (T, thresh) = cv2.threshold(darkgray, threshold, 255, cv2.THRESH_BINARY)
     contours = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
     contours = imutils.grab_contours(contours)
+
+    return contours, darkgray
+
+def apply_darkframe(image, contours, highlight=False):
+    outimage = image.copy()
+    # find the 'average' color of the input image
+    avg_color_per_row = numpy.average(image, axis=0)
+    avg_color = numpy.average(avg_color_per_row, axis=0)
+
     for c in contours:
         (x, y, w, h) = cv2.boundingRect(c)
         if False:
