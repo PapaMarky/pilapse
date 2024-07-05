ca694840f9a5d6ebd1a11bf68925a4142242c06a
Mark Dyer <heymarky@gmail.com>
2024-07-05 09:50:20 -0700

    Put boxes around movement (changes) in title frame.

--- pc2-motion-post.py
+++ pc2-motion-post.py
@@ -6,6 +6,7 @@ import sys
 from pathlib import Path
 from data_scaler import DataScaler
 import cv2
+import imutils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s|%(levelname)s|%(threadName)s|%(message)s')
 
@@ -33,7 +34,7 @@ class ClipMetadata(object):
         return self.header['fps'] if 'fps' in self.header else None
 
     def get_frame(self, i):
-        if i < 0 or i >= len(self.framedata):
+        if i < 0 or i >= self.nframes:
             message = f'bad frame index: {i} is not between 0 and {len(self.framedata)}'
             logging.error(message)
             return None
@@ -119,7 +120,6 @@ class RawClipProcessor(object):
         video_capture = cv2.VideoCapture(str(self.clip_path))
         # TODO check first frame fps, last frame fps and clip fps. 2 out of 3 wins
         self.fps = int(self.metadata.fps)
-        logging.info(f'No FPS in metadata, checking first and last frames.')
         fps_data = {}
         for frame in self.metadata.framedata:
             fps = frame['fps']
@@ -148,6 +148,7 @@ class RawClipProcessor(object):
             if not success:
                 break
             cv2.imwrite(self.frame_file_path(self.frame_count), frame)
+            frame = None
             self.frame_count += 1
         video_capture.release()
         logging.info(f'Wrote {self.frame_count} frames to {self.temp_dir.name}')
@@ -189,6 +190,7 @@ class RawClipProcessor(object):
             delta_data.append(delta)
             if i == 0:
                 min_mse = max_mse = mse
+                self.title_frame_number = i
                 continue
             if mse < min_mse:
                 min_mse = mse
@@ -219,6 +221,7 @@ class RawClipProcessor(object):
             y = int(self.yscaler.scale(delta_data[i]))
             self.delta_graph.append((x, y))
 
+
     def create_raw_video(self):
         clip_dir = self.temp_dir.parent
         discard = ''
@@ -231,18 +234,145 @@ class RawClipProcessor(object):
         size = (self.frame_width, self.frame_height)
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
         video_writer = cv2.VideoWriter()
+        if clip_name.exists():
+            clip_name.unlink()
         video_writer.open(str(clip_name), fourcc, int(self.fps), size)
-        frame_file_list = []
+        self.frame_file_list = []
         for frame in frame_files:
-            frame_file_list.append(frame)
-        frame_file_list.sort()
-        title_frame = None
-        if self.title_frame_number is not None:
-            title_frame = cv2.imread(self.frame_file_path(self.title_frame_number))
+            self.frame_file_list.append(frame)
+        self.frame_file_list.sort()
 
         metadata_offset = self.metadata.nframes - self.frame_count
         if self.metadata.nframes > self.frame_count:
             metadata_offset -= 1
+
+        def write_debug_image(typename, image):
+            outdir = clip_name.parent.parent
+            outdir = outdir.parent
+            image_file_basename = clip_name.with_suffix('').name
+            image_file_basename = outdir.joinpath(image_file_basename)
+            logging.info(f'DEBUG BASENAME: {image_file_basename}')
+            fname = f'{image_file_basename}_{typename}.png'
+            if os.path.exists(fname):
+                os.remove(fname)
+            cv2.imwrite(fname, image)
+
+        def mark_motion(background_frame, motion_frame):
+            logging.info(f'Marking Motion:')
+            marked_image = None
+            # scale the images to reduce processing time
+            logging.info(f'Marking Motion: Scaling images')
+            height, width, _ = background_frame.shape
+            scale = 2
+
+            scaled_height = int(height / scale)
+            background_frame_scaled = imutils.resize(background_frame.copy(), height=scaled_height)
+            write_debug_image('00_BACK', background_frame_scaled)
+            motion_frame_scaled = imutils.resize(motion_frame.copy(), height=scaled_height)
+            write_debug_image('01_MOTION', motion_frame_scaled)
+            logging.info(f'Marking Motion: diffing the images')
+            diff = background_frame_scaled.copy()
+            cv2.absdiff(background_frame_scaled, motion_frame_scaled, diff)
+
+            write_debug_image('10_DIFF', diff)
+            logging.info(f'Marking Motion: grayscale')
+            gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+            ## write_debug_image('20_GRAY', gray)
+            # increasing the size of differences so we can capture them all
+            # for i in range(0, 3):
+            logging.info(f'Marking Motion: dilate')
+            dilated = gray.copy()
+            for i in range(0, 5):
+               dilated = cv2.dilate(dilated, None, iterations=i + 1)
+            ## write_debug_image('30_DILATED', dilated)
+            logging.info(f'Marking Motion: threshold')
+            threshold = 70
+            (T, thresh) = cv2.threshold(dilated, threshold, 255, cv2.THRESH_BINARY)
+            logging.info(f'Marking Motion: contours')
+            # cnts = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
+            cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+            cnts = imutils.grab_contours(cnts)
+            # motion_data.contours = cnts
+            mindiff = 280
+            sMindiff = int(mindiff / scale)
+            logging.info(f'Marking Motion: adding {len(cnts)} rectangles')
+
+            added_rect_count = 0
+            first = True
+            for c in cnts:
+                if first:
+                    first = False
+
+                # fit a bounding box to the contour
+                (x, y, w, h) = cv2.boundingRect(c)
+                color = RED
+                if (w >= sMindiff or h >= sMindiff) and w < width and h < height:
+                    color = MAGENTA
+                    added_rect_count += 1
+                sx = int(scale * x)
+                sy = int(scale * y)
+                sw = int(scale * w)
+                sh = int(scale * h)
+                cv2.rectangle(motion_frame, (sx, sy), (sx + sw, sy + sh), color, thickness=2)
+            logging.info(f' - {added_rect_count} rects actually added')
+
+            return motion_frame
+
+        def create_title_frame():
+            logging.info(f'Creating title frame')
+            if self.title_frame_number is None:
+                logging.error(f'Title frame number not set. Cannot make title frame')
+                return None
+            title_frame = cv2.imread(self.frame_file_path(self.title_frame_number))
+
+            # find the motion-free frame closest to the title frame
+            frame = self.title_frame_number - 1
+            background_frame = None
+            EXTRA_MOTIONLESS = 3
+            n = 0
+            while frame >= metadata_offset:
+                fmeta = self.metadata.get_frame(frame + metadata_offset)
+                if not fmeta['motion']:
+                    if n >= EXTRA_MOTIONLESS:
+                        background_frame = frame
+                        break
+                    n += 1
+                else:
+                    n = 0
+                frame -= 1
+
+            if background_frame == None:
+                frame = self.title_frame_number + 1
+                n_frame_files = len(self.frame_file_list)
+                n = 0
+                while frame < n_frame_files - 1:
+                    fmeta = self.metadata.get_frame(frame + metadata_offset)
+                    if fmeta is None:
+                        logging.warning(f'fmeta: {fmeta}, frame: {frame}, metadata_offset: {metadata_offset}, nfiles: {n_frame_files}')
+                    elif not fmeta['motion']:
+                        if n >= EXTRA_MOTIONLESS:
+                            background_frame = frame
+                            break
+                        n += 1
+                    else:
+                        n = 0
+                    frame += 1
+            if background_frame == None:
+                logging.warning(f'No suitable background frame. Cannot mark motion in frame')
+
+            if background_frame is not None:
+                background_frame = cv2.imread(str(self.frame_file_list[background_frame]))
+                marked_frame = mark_motion(background_frame, title_frame)
+                if marked_frame is not None:
+                    pass
+                    # title_frame = marked_frame
+                else:
+                    logging.warning(f'marked_frame is None')
+
+            return title_frame
+
+        title_frame = create_title_frame()
+
         def draw_footer(frame_image, frame_number):
 
             metadata_frame = self.metadata.get_frame(metadata_offset + frame_number)
@@ -272,7 +402,7 @@ class RawClipProcessor(object):
 
         frame_number = 0
         timestamp_origin = (30, self.frame_height - 60)
-        for frame in frame_file_list:
+        for frame in self.frame_file_list:
             if title_frame is not None:
                 draw_footer(title_frame, self.title_frame_number)
                 video_writer.write(title_frame)
@@ -283,10 +413,14 @@ class RawClipProcessor(object):
 
             draw_footer(frame_image, frame_number)
             video_writer.write(frame_image)
+            frame_image = None
             frame_number += 1
         video_writer.release()
         # move the file from clip_dir to clip_dir.parent
-        shutil.move(clip_name, self.outdir)
+        outfile = self.outdir.joinpath(clip_name.name)
+        if outfile.exists():
+            outfile.unlink()
+        shutil.move(clip_name, outfile)
         logging.info(f' - Finished writing {clip_name}')
 
     def cleanup(self):
@@ -295,7 +429,7 @@ class RawClipProcessor(object):
         # delete the raw video?
         logging.info(f'Deleting {self.clip_path}')
         self.clip_path.unlink()
-        # logging.info(f'Deleteing {self.metadata_path}')
+        logging.info(f'Deleteing {self.metadata_path}')
         # self.metadata_path.unlink()
 
 
